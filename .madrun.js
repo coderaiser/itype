9ef07c4688c0df6bef441092cd9778e183d002ff
coderaiser <coderaiser@cloudcmd.io>
2019-12-13 11:52:18 +0200

    chore(itype) add madrun

---
+++ .madrun.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const {run} = require('madrun');
+
+module.exports = {
+    'test': () => 'tape test/*.js',
+    'watcher': () => 'nodemon -w test -w lib --exec',
+    'watch:test': () => run('watcher', 'npm test'),
+    'watch:coverage': () => run('watcher', 'npm run coverage'),
+    'lint': () => 'putout lib test .madrun.js',
+    'fix:lint': () => run('lint', '--fix'),
+    'coverage': () => 'nyc npm test',
+    'report': () => 'nyc report --reporter=text-lcov | coveralls',
+};
+
